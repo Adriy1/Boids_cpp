9eadd88a2f091d5949f0dbbb6b04a7359042d0e6
pieta <adrien.piet@phelma.genoble-inp.fr>
2019-03-22 19:11:53 +0100

    ajout correct randomness

--- Boids.cpp
+++ Boids.cpp
@@ -1,10 +1,12 @@
 #include "Boids.h"
 #include "Boid.h"
 #include <cmath>
+#include <ctime>
 #include <stdlib.h>
 #include "BoidSimulator.h"
 
 Boids::Boids(int nbBoid){
+  srand((unsigned)time(0)); 
   for(int i=0;i<nbBoid;i++){
     // listeBoid.push_back(Boid(Vecteur(rand()%GLOBAL_CONST_WIDTH,rand()%GLOBAL_CONST_HEIGHT),Vecteur(1,0)));
     listeBoid.push_back(Boid(Vecteur(rand()%GLOBAL_CONST_WIDTH,rand()%GLOBAL_CONST_HEIGHT),Vecteur(rand()%10-5,rand()%10-5)));
